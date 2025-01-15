3d1c1cd494234df37b6d6781f6a9273ecf339c43
Arinhakeo <9295@holbertonstudents.com>
2025-01-15 16:39:11 +0100

    reviewFizz

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -17,13 +17,13 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:   # Check FizzBuzz first
+        if (i % 3) == 0 and (i % 5) == 0:   # Check for FizzBuzz first
             tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:                  # Then check Fizz
+        elif (i % 3) == 0:                  # Then check for Fizz
             tmp_result.append("Fizz")
-        elif (i % 5) == 0:                  # Then check Buzz
+        elif (i % 5) == 0:                  # Then check for Buzz
             tmp_result.append("Buzz")
-        else:                               # Otherwise and add the number
+        else:                               # Otherwise, just add the number
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
@@ -37,4 +37,3 @@ if __name__ == '__main__':
 
     number = int(sys.argv[1])
     fizzbuzz(number)
-    
